174eb03dc651fbd45e28ea5d435d413b5ab3ab95
Ademola <104240728+acewrld@users.noreply.github.com>
2022-11-16 14:52:37 +0100

    Update errors.c

--- errors.c
+++ errors.c
@@ -22,7 +22,6 @@ void _eputs(char *str)
 /**
  * _eputchar - writes the character c to stderr
  * @c: The character to print
- *
  * Return: On success 1.
  * On error, -1 is returned, and errno is set appropriately.
  */
